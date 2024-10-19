33fe8ca54c3d811674264183ea08f97e32a62ece
lamia <lamianisha3@gmai..com>
2024-10-19 09:15:51 +0600

    after implementing the sign out btn using ternary operator and also implemented the user data working

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,61 @@
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase.init";
 import { GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
 const Login = () => {
     const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
 
+    const [data, setData] = useState([]);
+    // const [user,setUser]=useState();
+
     const handleGoogleSignIn = () => {
 
         signInWithPopup(auth, provider)
             .then(result => {
-                const user = result.user;
-                console.log(user);
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                setData(loggedUser);
             })
             .catch(error => {
                 console.log('errror', error.message);
+
             });
+    };
+
+    const handleSignOut = () => {
+
+        signOut(auth)
+            .then(result => {
+                console.log(result);
+                setData(null);
+            })
+            .catch(error => {
+                console.log(error.message);
+            });
+
     };
 
 
     return (
         <div>
-            <button onClick={handleGoogleSignIn} className="btn btn-outline">Google Login</button>
+
+            {
+                data ?
+                    <button className="btn" onClick={handleSignOut}>SignOut</button> :
+                    <button className="btn" onClick={handleGoogleSignIn}>Login</button>
+            }
+
+            <div>
+                {data && <div>
+                    <h1>Name: {data.displayName} </h1>
+                    <img src={data.photoURL} alt="" />
+                </div>}
+            </div>
+
+
+
         </div>
     );
 };
